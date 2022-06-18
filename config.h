448109fd6f75b27b2a0c9b759642dc39069b42e4
GlitchPhoenix98 <GlitchPhoenix98>
2022-06-18 14:55:39 -0400

    Fixed pulsemixer

--- config.h
+++ config.h
@@ -61,7 +61,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *web[] = { "firefox", NULL };
-static const char *audio[] = { "st pulsemixer", NULL };
+static const char *audio[] = { "st", "pulsemixer",  NULL };
 static const char *lock[] = { "slock", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
