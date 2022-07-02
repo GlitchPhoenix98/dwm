0116f7f0b031f368df8d44379fa36d10416fe787
glitchphoenix98 <glitchphoenix98>
2022-07-01 21:54:27 -0400

    using URxvt

--- config.h
+++ config.h
@@ -64,9 +64,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "urxvtc", NULL };
 static const char *web[] = { "firefox", NULL };
-static const char *audio[] = { "alacritty", "--command", "pulsemixer",  NULL };
+static const char *audio[] = { "urxvtc", "-name", "pulsemixer", "-e", "sh", "-c", "pulsemixer",  NULL };
 static const char *lock[] = { "slock", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
