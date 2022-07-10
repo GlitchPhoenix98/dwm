7b4015578f71cce1ad4f2923cbc0f768447041f8
glitchphoenix98 <glitchphoenix98>
2022-07-10 19:24:23 -0400

    updated config

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvtc", NULL };
 static const char *web[] = { "firefox", NULL };
-static const char *audio[] = { "urxvtc", "-name", "pulsemixer", "-e", "sh", "-c", "pulsemixer",  NULL };
+static const char *audio[] = { "pavucontrol", "-t", "1",  NULL };
 static const char *lock[] = { "slock", NULL };
 /* Optional Laptop Backlight bindings.
 static const char *brightup[] = { "xbacklight", "-inc", "10", NULL };
